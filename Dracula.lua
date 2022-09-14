304d1b54aee55c407699eda82b706b8efd581a01
Stephan Langenau <55900780+kaeptnkrunch@users.noreply.github.com>
2022-09-14 21:11:31 +0200

    Update Dracula.lua Added the standard color palette for copy and paste!

--- Dracula.lua
+++ Dracula.lua
@@ -8,7 +8,20 @@
     #                                                                                                  #
     ####################################################################################################]]
 --
--- Colors can be changed easily by changing the Hex Code!
+--[[Dracula standard colors are:
+    
+    Palette	        HEX	
+    Background	  #282a36	
+    Current Line	#44475a	
+    Foreground	  #f8f8f2	
+    Comment	      #6272a4	
+    Cyan	        #8be9fd	
+    Green	        #50fa7b	
+    Orange	      #ffb86c	
+    Pink	        #ff79c6	
+    Purple	      #bd93f9	
+    Red	          #ff5555	
+    Yellow	      #f1fa8c]]
 --
 --
 -- Many colors support 'active', 'inactive', and 'disabled' states.
